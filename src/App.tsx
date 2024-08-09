541d2a2a42605977faabe7b50fae05f4f22932d4
elyasprba <elyasprba@Ts-MacBook-Pro.local>
2024-08-09 16:33:13 +0700

    feat: update content

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <>
-      <h3>Success implement pipeline CI/CD using Github Actions</h3>
-      <div>@elyasprba</div>
+      <div>
+        <h3>Success implement pipeline CI/CD using Github Actions</h3>
+        <div>@elyasprba</div>
+      </div>
     </>
   );
 }
