98222a8e116201d34c80f953bd1e7e94d9f3883f
Elyas Purba Prastiya <95122504+elyasprba@users.noreply.github.com>
2024-08-09 16:36:53 +0700

    Feat/update content (#11)
    * feat: update content
    
    * feat: update content
    
    * feat: update content
    
    ---------
    
    Co-authored-by: elyasprba <elyasprba@Ts-MacBook-Pro.local>

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <>
-      <h3>Success implementasi pipeline CI/CD menggunakan Github Actions</h3>
-      <div>@elyasprba</div>
+        <h3>Success implement pipeline CI/CD using Github Actions</h3>
+        <div>@elyasprba</div>
     </>
   );
 }
