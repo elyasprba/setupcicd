47fba744d0866ac10bffd1dddf6f46612eada344
elyasprba <elyasprba@Ts-MacBook-Pro.local>
2024-08-11 11:20:26 +0700

    feat: setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <>
-      <h4>Success implement pipeline CI/CD using Github Actions</h4>
-      <p>@elyasprba</p>
+      <div className="bg-red-400">
+        <h4>Success implement pipeline CI/CD using Github Actions</h4>
+        <p>@elyasprba</p>
+      </div>
     </>
   );
 }
