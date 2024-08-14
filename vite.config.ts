a09037a940e234263dda96de458f1893237600ec
elyasprba <elyasprba@Ts-MacBook-Pro.local>
2024-08-14 18:37:10 +0700

    fix: blank page

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/setupcicd/', // Add only this section
+  base: '/setupcicd/', // Add only this section
   server: {
     host: '0.0.0.0',
     port: 4200,
