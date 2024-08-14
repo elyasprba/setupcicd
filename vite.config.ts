78589a8c074b5e4b96d00086b81bc0cef397df5f
elyasprba <elyasprba@Ts-MacBook-Pro.local>
2024-08-14 18:34:28 +0700

    feat/setup-docker

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/setupcicd/', // Add only this section
+  // base: '/setupcicd/', // Add only this section
   server: {
+    host: '0.0.0.0',
+    port: 4200,
+  },
+  preview: {
     port: 4200,
   },
 });
